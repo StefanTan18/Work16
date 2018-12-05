090fe42a7ae9418e76e528e270b56da9429ac922
Stefan Tan <stan3@stuy.edu>
2018-12-04 21:38:07 -0500

    finished main.c

--- main.c
+++ main.c
@@ -9,33 +9,38 @@ int main() {
   key_t key;
   int shmid;
   char *data;
-  char r[2];
-  char str[200];
+  char r[200];
   
-  key = ftok("/home/Work16/file", 'R');
+  key = ftok("file", 'R');
   shmid = shmget(key, 200, 0644 | IPC_CREAT);
   data = shmat(shmid, (void *)0, 0);
 
-  if (data)
+  if (!data[0])
     printf("Shared Memory Segment was just created\n");
   else {
     printf("Segment contains: %s\n", data);
   }
   printf("Do you want to change the data in the segment? (y/n)\n");
-  fgets(r, 2, stdin);
-  if (strcmp(r, "y") == 0) {
+  fgets(r, 200, stdin);
+  if (!strcmp(r, "y\n")) {
     printf("Enter a new string:\n");
-    fgets(str, 200, stdin);
-    strncpy(data, str, 200);
+    fgets(r, 200, stdin);
+    strcpy(data, r);
   } 
 
+  printf("Segment contains: %s\n", data);
+  
   printf("Do you want to delete the segment? (y/n)\n");
-  fgets(r, 2, stdin);
-  if (strcmp(r, "y") == 0) {
+  fgets(r, 200, stdin);
+  
+  if (!strcmp(r, "y\n")) {
     shmdt(data);
     shmctl(shmid, IPC_RMID, NULL);
+    printf("Segment deleted.\n");
   }
-  
+  else {
+    printf("Segment was not deleted.\n");
+  }
   return 0;
 }
   
